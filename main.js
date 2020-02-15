2449eba26d44a13145b2e8134e4d2240da927461
stefan.jovanovic <stefan.jovanovic@zone39-wb.cs.ucalgary.ca>
2020-02-15 14:21:06 -0700

    working initial state

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var path = require('path');
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
